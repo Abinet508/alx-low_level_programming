c93f3ba6c8c2e0bed8f33d1d56afaacb3806af04
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-08-27 12:46:05 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,8 @@ int main(void)
 int firstLetter, n;
 for (firstLetter = 97, n = 1; n < 27; firstLetter++, n++)
 {
-
+if (firstLetter == 101 || firstLetter == 113)
+continue;
 putchar(firstLetter);
 }
 putchar(10);
