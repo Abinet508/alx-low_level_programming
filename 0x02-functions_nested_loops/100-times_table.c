e68f04a0206db5e4d4b18b152251b2d36e6d12ea
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-08-31 13:34:23 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,49 +7,38 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k,product;
-	j = n + 1;
-	for (i = 0; i < j; i++)
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (k = 0; k < j; k++)
+		for (num = 0; num <= n; num++)
 		{
-			if (n > 15)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				break;
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-			else
-			{
-				product = i * k;
-				if (k == 0)
-		       		{
-					_putchar('0' + product);
-				}
-				else if (product >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' +  (product % 10));
-				}
-				else if (product >= 100)
-				{
-	       
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				if (k != n)
-				{
-					_putchar(',');
-				}
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-
