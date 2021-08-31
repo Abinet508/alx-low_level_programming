0c8d92e13d2972af969887ef08bd260942f8cc86
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-08-31 15:35:02 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,13 +8,12 @@
 int main(void)
 {
 	int n = 0;
-	char str_hbt[9] = "Holberton";
+	char str_hbt[9] = "Holberton\n";
 
 	while (n < 9)
 	{
 		_putchar(str_hbt[n]);
 		n += 1;
 	}
-	_putchar('\n');
 	return (0);
 }
