b6c7dbf352df2968fbe99df8595d06d92a5569f0
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-08-31 19:59:26 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@
  *       used in the 0x02-functions_nested_loops directory.
  */
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -23,16 +23,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+
