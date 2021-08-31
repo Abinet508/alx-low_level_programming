af3de1d4b4c06bc0a98044df1c32d3237338380c
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-08-31 16:46:16 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -9,4 +9,4 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
