ea6b52447d98233b09e396fff67d8001b4117c6e
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-02 12:54:59 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
- * _isupper - check for upper case letter
- * @c : character to check the case
- * Return:0 or 1
- */
+* _isupper - check for upper case letter
+* @c : character to check the case
+* Return:0 or 1
+*/
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+return (c >= 65 && c <= 90);
 
 }
