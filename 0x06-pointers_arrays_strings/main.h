d45d989781ad9ad13e64c829ae88fee3e2127d4b
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-08 15:04:28 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,5 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
