bf6ec1153781378d310dcd1ed41b4a90d0259fa1
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-09 07:17:57 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int byteCount;
-
-	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
-		dest[byteCount] = src[byteCount];
-		for (; byteCount < n; byteCount++)
-			dest[byteCount] = '\0';
-
-	return (dest);
+int byteCount;
+for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+{
+dest[byteCount] = src[byteCount];
+}
+for (; byteCount < n; byteCount++){
+	dest[byteCount] = '\0';
+}
+return (dest);
 }
