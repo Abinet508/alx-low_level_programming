1c2b26757113151429e3f5ece3d075ec48ccbecd
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-09 07:33:40 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,30 +9,31 @@
  */
 char *cap_string(char *s)
 {
-	int count;
+int count;
 
 /*  scan through string */
-	count = 0;
-	while (s[count] != '\0')
-	{/* if next character after count is a char , capitalise it */
-		if (s[0] >= 97 && s[0] <= 122)
-		{
-			s[0] = s[0] - 32;
-		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-		    || s[count] == ',' || s[count] == ';' || s[count] == '.' 
-			|| s[count] == '!' || s[count] == '?'|| s[count] == '"' 
-			|| s[count] == '(' || s[count] == ')'|| s[count] == '{' 
-			|| s[count] == '}')
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
-			{
-				s[count + 1] = s[count + 1] - 32;
-			}
-		}
-		count++;
-	}
-	return (s);
+count = 0;
+/* if next character after count is a char , capitalise it */
+while (s[count] != '\0')
+{
+if (s[0] >= 97 && s[0] <= 122)
+{
+s[0] = s[0] - 32;
+}
+if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+|| s[count] == ',' || s[count] == ';' || s[count] == '.' 
+|| s[count] == '!' || s[count] == '?'|| s[count] == '"' 
+|| s[count] == '(' || s[count] == ')'|| s[count] == '{' 
+|| s[count] == '}')
+{
+if (s[count + 1] >= 97 && s[count + 1] <= 122)
+{
+s[count + 1] = s[count + 1] - 32;
+}
+}
+count++;
+}
+return (s);
 }
 int main(void)
 {
