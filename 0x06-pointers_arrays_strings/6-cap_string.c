115a28592d59eb0f16e44929648a70c40da41eec
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-09 07:34:17 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -35,13 +35,3 @@ count++;
 }
 return (s);
 }
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
-}
