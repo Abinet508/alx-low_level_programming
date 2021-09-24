8a4410463539179420a7e9d22135009dbafff353
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-24 12:15:48 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	ptr = malloc(sizeof(ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
 		return (NULL);
 
