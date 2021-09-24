ce09ecef62fbc0372d33f3725dd1206b0a1c39c8
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-24 12:07:33 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,53 @@
 #include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, len = 0, lens1;
-char *str;
+	char *ptr;
+	int num, len, i, j;
 
-if (s1 != NULL)
-for (i = 0; s1[i] != '\0'; i++)
-len++;
-lens1 = len;
-if (s2 != NULL)
-{
-if (sizeof(s2) > n)
-for (i = 0; i < n; i++)
-len++;
-else
-for (i = 0; s2[i] != '\0'; i++)
-len++;
-}
-str = malloc(sizeof(char) * (len + 1));
-if (str == NULL)
-return (NULL);
-str[len] = '\0';
-if (s1 != NULL)
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-if (s2 != NULL)
-for (; i < len; i++)
-str[i] = s2[i - lens1];
-return (str);
+	num = n;
+
+	if (s1 == NULL) /* account for NULL strings */
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0) /* account for negative n bytes */
+		return (NULL);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
 }
