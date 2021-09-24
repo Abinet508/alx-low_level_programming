eacc0d22155796e6926ed66e0a347f4a083b063b
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-09-24 11:57:13 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,35 +9,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+unsigned int i, len = 0, lens1;
+char *str;
 
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
-
-if (n < len2)
-s = malloc(sizeof(char) * (len1 + n + 1));
-else
-s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-if (!s)
-return (NULL);
-
-while (i < len1)
+if (s1 != NULL)
+for (i = 0; s1[i] != '\0'; i++)
+len++;
+lens1 = len;
+if (s2 != NULL)
 {
-s[i] = s1[i];
-i++;
+if (sizeof(s2) > n)
+for (i = 0; i < n; i++)
+len++;
+else
+for (i = 0; s2[i] != '\0'; i++)
+len++;
 }
-
-while (n < len2 && i < (len1 + n))
-s[i++] = s2[j++];
-
-while (n >= len2 && i < (len1 + len2))
-s[i++] = s2[j++];
-
-s[i] = '\0';
-
-return (s);
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+str[len] = '\0';
+if (s1 != NULL)
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
+if (s2 != NULL)
+for (; i < len; i++)
+str[i] = s2[i - lens1];
+return (str);
 }
