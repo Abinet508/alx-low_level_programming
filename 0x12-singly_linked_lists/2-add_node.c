b41af65899cc8dadf53964df9b5f2c3dbe111f5d
Abinet505 <abinatmail@gmail.com>
2021-10-08 10:57:35 +0300

    fixed 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <string.h>
-
 /**
  *add_node - function to add item at the beginning of a linked_list
  *@head: head pointer to linked_list
@@ -8,8 +7,6 @@
  *
  *Return: address of new element
  */
-
-
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
