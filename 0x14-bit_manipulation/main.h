ecdc35d9074b7146412268cd807bf075831df069
Abnet Tesfu Asrat <57164101+Abinet508@users.noreply.github.com>
2021-10-14 20:55:24 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /**/
 #include <stdlib.h>
-
 /* Function Prototypes */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -16,5 +14,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
 #endif /* MAIN_H */
