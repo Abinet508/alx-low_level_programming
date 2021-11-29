18adb44e94d6b56fba3b5fe9c76fc9d74b799fc4
Abnet Tesfu Asrat <abinatmail@gmail.com>
2021-11-29 17:39:32 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Abinet508/alx-low_level_programming/blob/main/0x18-dynamic_libraries/path.so?raw=true
-export LD_PRELOAD=/tmp/path.so
+gcc *.o -shared -o 4libc.so
+LD_PRELOAD=$PWD/4libc.so
