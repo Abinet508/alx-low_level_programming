c62a798a09bec6e19ead98bd36e3a8debeccedd7
Abnet Tesfu Asrat <abinatmail@gmail.com>
2021-11-29 17:38:33 +0300

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
-gcc -shared *.o -o liball.so
+gcc -Wall *.c -c -fPIC
+gcc *.o -shared -o liball.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
